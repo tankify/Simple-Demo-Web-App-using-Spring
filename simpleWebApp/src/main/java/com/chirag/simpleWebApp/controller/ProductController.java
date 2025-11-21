b3f570b5d98d22f54eec4eb48ec9d6fadd23a3ae
Chirag Tank <tankchirag22@gmail.com>
2025-11-21 13:34:28 +0530

    Working with GET API requests

--- simpleWebApp/src/main/java/com/chirag/simpleWebApp/controller/ProductController.java
+++ simpleWebApp/src/main/java/com/chirag/simpleWebApp/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.chirag.simpleWebApp.controller;
 import com.chirag.simpleWebApp.model.Product;
 import com.chirag.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class ProductController {
     @RequestMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
     }
 
 }
